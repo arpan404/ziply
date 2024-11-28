91f3c9730b2197387b2863fa310962c9b34b89d4
Arpan Bhandari <itsmearpan1@gmail.com>
2024-11-28 17:28:11 -0600

    #add: Added help feature for ziply

--- src/parser.cpp
+++ src/parser.cpp
@@ -11,7 +11,23 @@ void Parser::parse(int argc, char *argv[], std::string *const fileName, std::str
     {
         if (std::string(argv[1]) == "--help")
         {
-            std::cout << "Help";
+            std::cout << "\nZiply Command Options:\n";
+
+            std::cout << "\nMandatory:\n";
+            std::cout << "-f  > File path or name to convert.\n";
+
+            std::cout << "\nOptional:\n";
+            std::cout << "-o  > Output file path or name (extension is ignored if provided).\n";
+            std::cout << "-r  > Resolution for output video (480p, 720p, 1080p, 1440p, or 4k).\n";
+            std::cout << "-p  > Secret key for encryption (default: 'ziplySecret').\n";
+            std::cout << "-m  > Processing mode: 'cpu-single' (single core), 'cpu-multi' (all cores), 'gpu' (GPU or CPU fallback), 'gpu-cpu' (all cores of CPU & GPU).\n";
+            std::cout << "-c  > Compression prevention (Pixel to bit ratio, default: 1). Higher values increase output length, suitable for uploading to compression-based platforms.\n";
+
+            std::cout << "\nExamples:\n";
+            std::cout << "ziply -f example.zip\n"
+                      << "\tConverts 'example.zip' to 'example.mp4'.\n";
+            std::cout << "ziply -f example.png -o zipled -r 1080p -p ziplySecret2 -m gpu -c 8\n"
+                      << "\tConverts 'example.png' to 'zipled.mp4' at 1080p resolution, encrypted with key 'ziplySecret2', using GPU, with pixel-to-bit ratio of 8.\n";
         }
         else
         {
